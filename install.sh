a2cd09370f861d3c61030adced88ea66fa6706b3
Benedikt Reiser <benedikt.reiser@innoactive.de>
2016-05-27 01:02:57 +0200

    added helper script to install sublime configuration

---
+++ install.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# This install script is based on [PaulMillr's](https://github.com/paulmillr) excellent install script
+
+# check if brew is installed already, if it isn't prompt the user to do so
+# If we on OS X, install homebrew and tweak system a bit.
+if [[ `uname` == 'Darwin' ]]; then
+  which -s brew
+  if [[ $? != 0 ]]; then
+    echo 'Installing Homebrew...'
+      ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+      brew update
+  fi
+
+  # now that we're certain we got brew installed, let's install GNU stow
+  # GNU stow is a symlink manager and can help us
+  which -s stow
+  if [[ $? != 0 ]]; then
+    echo 'Installing GNU Stow'
+    brew install stow
+  fi
+
+  # symlink the preferences for sublime, to do so, remove all local settings first and create a symlink instead
+  echo "Updating Sublime Text 3 Configuration"
+
+  SUBLIMEPATH=~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+  CONFIGPATH=$(pwd)/sublime/Packages/User
+  FILES="$CONFIGPATH/*.sublime-settings"
+  for file in $FILES
+  do
+    filename=$(basename "$file")
+    # take action on each file. $f store current file name
+    # check whether the file already exists in the sublime text folder.
+    if [ -L "$SUBLIMEPATH/$filename" ]; then
+      # if it is a symlink, overwrite it
+      echo "Symlink to config file $filename already exists. Overwriting ..."
+    elif [ -f "$SUBLIMEPATH/$filename" ]; then
+      # if it is a file, back it up
+      echo "Config File $filename already exists. Backing up ..."
+      cp "$SUBLIMEPATH/$filename" "$SUBLIMEPATH/$filename.bak"
+    fi
+    # now replace the file / symlink with a symlink into our repo
+    ln -sf "$file" "$SUBLIMEPATH"
+  done
+
+  echo 'Tweaking OS X...'
+    source 'etc/osx.sh'
+
+  # http://github.com/sindresorhus/quick-look-plugins
+  echo 'Installing Quick Look plugins...'
+    brew tap phinze/homebrew-cask
+    brew install caskroom/cask/brew-cask
+    brew cask install suspicious-package quicklook-json qlmarkdown qlstephen qlcolorcode
+fi
