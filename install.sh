bdd2c6fce55e596ce212c5c9c716587d56764ad6
Benedikt Reiser <benedikt.reiser@innoactive.de>
2016-05-27 20:42:00 +0200

    made osx defaults optional

--- install.sh
+++ install.sh
@@ -56,8 +56,13 @@ if [[ `uname` == 'Darwin' ]]; then
     ln -sf "$file" "$SUBLIMEPATH"
   done
 
-  echo 'Tweaking OS X...'
-    source "$(pwd)/osx-defaults.sh"
+  echo "Do you wish to install sensible defaults for OSX (this might take some time)?"
+  select yn in "Yes" "No"; do
+      case $yn in
+          Yes ) echo 'Tweaking OS X...'; source "$(pwd)/osx-defaults.sh" break;;
+          No ) echo 'Not tweaking OS X. Just re-run install.sh if you change your mind'; break;;
+      esac
+  done
 
   # http://github.com/sindresorhus/quick-look-plugins
   echo 'Installing Applications with Homebrew ...'
