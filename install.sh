679d38180030bbef030d037eb6ddbb239601a43c
Benedikt Reiser <benedikt.reiser@innoactive.de>
2016-05-27 16:13:52 +0200

    install script now also symlinking dotfiles and setting up zgen

--- install.sh
+++ install.sh
@@ -12,13 +12,26 @@ if [[ `uname` == 'Darwin' ]]; then
       brew update
   fi
 
+  # install the powerline fonts
+  echo 'Installing Powerline fonts ...'
+    source ./fonts/install.sh
+
   # now that we're certain we got brew installed, let's install GNU stow
   # GNU stow is a symlink manager and can help us
   which -s stow
   if [[ $? != 0 ]]; then
     echo 'Installing GNU Stow'
-    brew install stow
+      brew install stow
   fi
+
+  # now symlink the configuration files / dotfiles for zsh to our home folder
+  echo 'Symlinking global dotfiles'
+    stow -v -t ~ -d system .
+  echo 'Symlinking zsh dotfiles'
+    stow -v -t ~ -d zsh .
+
+  # setup zsh with all kinds of plugins using zgen
+   source ~/.zgen-setup
 
   # symlink the preferences for sublime, to do so, remove all local settings first and create a symlink instead
   echo "Updating Sublime Text 3 Configuration"
@@ -48,5 +61,5 @@ if [[ `uname` == 'Darwin' ]]; then
 
   # http://github.com/sindresorhus/quick-look-plugins
   echo 'Installing Applications with Homebrew ...'
-  brew bundle
+    brew bundle
 fi
