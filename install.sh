9e4b1809419e964f5d01a5a37410114331bff69f
Benedikt Reiser <benedikt.reiser@gmail.com>
2017-01-06 02:25:06 +0100

    moved helpers to root and fixed install to work from any dir

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+DOTFILES_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # load installation helpers
-source ./install/helpers.sh
+source $DOTFILES_ROOT/helpers.sh
 # find out which platform we're running on
 pf=`platforms`
 
@@ -14,40 +15,40 @@ success "$(printf "%b%s%b successfully updated" $light_green "Git Submodules" $r
 info "$(printf "Installing / Checking %b%s%b" $light_green "Powerline Fonts" $reset_color)"
 if [[ "$pf" == 'windows' ]]; then
   # check if fonts are maybe already installed
-  source "./windows/bootstrap/fonts.sh"
+  source "$DOTFILES_ROOT/windows/bootstrap/fonts.sh"
 else
-  source ./fonts/install.sh
+  source $DOTFILES_ROOT/fonts/install.sh
 fi
 success "$(printf "%b%s%b correctly installed" $light_green "Powerline Fonts" $reset_color)"
 
 # Package Management
 # run any platform specific package management tasks
-if [[ -f "./${pf}/bootstrap/packages.sh" ]]; then
+if [[ -f "$DOTFILES_ROOT/${pf}/bootstrap/packages.sh" ]]; then
   info "$(printf "Installing / Updating Packages using %b%s%b" $light_green "Package Manager" $reset_color)"
-  source "./${pf}/bootstrap/packages.sh"
+  source "$DOTFILES_ROOT/${pf}/bootstrap/packages.sh"
 fi
 
 # Symlink / Dotfile Management
 info "$(printf "Taking care of dotfiles through %b%s%b" $light_green "symlinking" $reset_color)"
-if [[ -f "./${pf}/bootstrap/dotfiles.sh" ]]; then
+if [[ -f "$DOTFILES_ROOT/${pf}/bootstrap/dotfiles.sh" ]]; then
   info "$(printf "Installing dotfiles to %b\$HOME%b=%b%s%b for %b\$OSTYPE%b=%b%s%b" \
       $light_red $reset_color \
       $green "$HOME" $reset_color \
       $light_red $reset_color \
       $light_yellow "$pf" $reset_color)"
-  source "./${pf}/bootstrap/dotfiles.sh"
+  source "$DOTFILES_ROOT/${pf}/bootstrap/dotfiles.sh"
 fi
 
 # symlink the preferences for sublime, to do so, remove all local settings first and create a symlink instead
-if [[ -f "./${pf}/bootstrap/sublime.sh" ]]; then
+if [[ -f "$DOTFILES_ROOT/${pf}/bootstrap/sublime.sh" ]]; then
   info "$(printf "Synchronizing %b%s%b specific settings" $light_green "Sublime Text 3" $reset_color)"
-  source ./install/sublime.sh
+  source $DOTFILES_ROOT/install/sublime.sh
 fi
 
 # run any platform specific bootstrap scripts (e.g. osx-defaults on macos)
-if [[ -f "./${pf}/bootstrap/platform.sh" ]]; then
+if [[ -f "$DOTFILES_ROOT/${pf}/bootstrap/platform.sh" ]]; then
   info "$(printf "Running platform-specific scripts for %b%s%b" $light_green $pf $reset_color)"
-  source "./${pf}/bootstrap/platform.sh"
+  source "$DOTFILES_ROOT/${pf}/bootstrap/platform.sh"
 fi
 
 info "Updating bash / zsh configuration"
