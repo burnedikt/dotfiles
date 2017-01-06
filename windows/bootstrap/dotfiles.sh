8d52b455d02d5341c0221b2652992de61b2f0b2e
Benedikt Reiser <benedikt.reiser@gmail.com>
2017-01-06 02:34:59 +0100

    editorconfig and functions now cross-platform

--- windows/bootstrap/dotfiles.sh
+++ windows/bootstrap/dotfiles.sh
@@ -2,13 +2,13 @@
 
 # symlink all required files
 link_dotfiles() {
-  # this is the path to the parent folder to the one in which this file resides
-  local BASE_SYMLINK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
   local overwrite_all=false backup_all=false skip_all=false
   # git
-  link_dotfiles_from_folder $BASE_SYMLINK_DIR/git
+  link_dotfiles_from_folder $DOTFILES_ROOT/windows/git
   # bash
-  link_dotfiles_from_folder $BASE_SYMLINK_DIR/bash
+  link_dotfiles_from_folder $DOTFILES_ROOT/windows/bash
+  # cross-platform dotfiles
+  link_dotfiles_from_folder $DOTFILES_ROOT/cross-platform
 }
 # go ahead
 link_dotfiles
