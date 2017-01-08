34772f035ee0230528b8d461b8464bbecb0f57fa
Benedikt Reiser <benedikt.reiser@gmail.com>
2017-01-08 15:26:44 +0100

    fixed aliases not loading on windows

--- windows/bootstrap/dotfiles.sh
+++ windows/bootstrap/dotfiles.sh
@@ -5,10 +5,10 @@ link_dotfiles() {
   local overwrite_all=false backup_all=false skip_all=false
   # git
   link_dotfiles_from_folder $DOTFILES_ROOT/windows/git
-  # bash
-  link_dotfiles_from_folder $DOTFILES_ROOT/windows/bash
   # cross-platform dotfiles
   link_dotfiles_from_folder $DOTFILES_ROOT/cross-platform
+  # (git-)bash
+  link_dotfiles_from_folder $DOTFILES_ROOT/windows/bash
 }
 # go ahead
 link_dotfiles
